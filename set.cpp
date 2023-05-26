7f66b9f0a07bfb30a0830c5a11939cc4266794f4
McNeelinet <lavelin.sava@gmail.com>
2023-05-26 15:19:53 +0300

    Set [almost] implemented. ?set operations?

--- set.cpp
+++ set.cpp
@@ -8,91 +8,151 @@ Iterator<T>::Iterator(const set<T>& container_obj) : cur_node(container_obj.get_
 
 /* Конструкторы */
 template <class T>
-set<T>::set()
-{
+set<T>::set() : first(nullptr), last(nullptr) {}
 
+template <class T>
+set<T>::set(const set<T>& s) : first(nullptr), last(nullptr)
+{
+    for (Iterator<T> iterator = s.iterator_begin(); !iterator.is_end(); ++iterator)
+        this->add(iterator.value());
 }
 
 template <class T>
-set<T>::set(const set<T>& s)
+set<T>::set(set<T>&& s) : first(s.get_first()), last(s.get_last())
 {
-
+    s.first = nullptr;
+    s.last = nullptr;
 }
 
 template <class T>
-set<T>::set(set<T>&& s)
+set<T>::set(std::initializer_list<T> lst) : first(nullptr), last(nullptr)
 {
-
-}
-
-template <class T>
-set<T>::set(std::initializer_list<T> lst)
-{
-
+    for (const T& value : lst)
+        this->add(value);
 }
 
 /* Полезное */
 template <class T>
 int set<T>::get_length() const
 {
+    int i = 0;
+    for (Iterator<T> iterator = this->iterator_begin(); !iterator.is_end(); ++iterator)
+        i++;
 
+    return i;
 }
 
 template <class T>
 bool set<T>::contains(const T& elem) const
 {
-
+    for (Iterator<T> iterator = this->iterator_begin(); !iterator.is_end(); ++iterator)
+        if (iterator.value() == elem)
+            return true;
+    return false;
 }
 
 template <class T>
 T* set<T>::to_array() const
 {
+    T* array = new T[this->get_length()];
+    int i = 0;
+    for (Iterator<T> iterator = this->iterator_begin(); !iterator.is_end(); ++iterator) {
+        array[i] = iterator.value();
+        i++;
+    }
 
+    return array;
 }
 
 /* Получение итераторов */
 template <class T>
 Iterator<T> set<T>::iterator_begin() const
 {
+    Iterator<T> iterator(*this);
 
+    return iterator;
 }
 
 template <class T>
 Iterator<T> set<T>::iterator_end() const
 {
+    Iterator<T> iterator;
 
+    for (iterator = this->iterator_begin(); !iterator.is_end(); ++iterator);
+
+    return iterator;
 }
 
 /*        Getter'ы        */
 template <class T>
 node<T>* set<T>::get_first() const
 {
-
+    return this->first;
 }
 
 template <class T>
 node<T>* set<T>::get_last() const
 {
-
+    return this->last;
 }
 
 /* Манипуляции с размером */
 template <class T>
 void set<T>::add(const T& elem)
 {
+    if (!this->contains(elem)) {
+        node<T>* newNode = new node<T>;
+        newNode->content = elem;
+        newNode->next = nullptr;
 
+        if (this->first == nullptr)
+            this->first = newNode;
+        else
+            this->last->next = newNode;
+        this->last = newNode;
+    }
+    else
+        throw std::invalid_argument("Cannot add dublicate values");
 }
 
 template <class T>
 void set<T>::remove(const T& elem)
 {
+    if (this->contains(elem)) {
+        node<T>* previous = this->first;
+        node<T>* current = this->first;
 
+        if (current->content != elem) {
+            while (current->content != elem) {
+                previous = current;
+                current = previous->next;
+            }
+            if (current->next == nullptr)
+                this->last = previous;
+            previous->next = current->next;
+            delete current;
+        }
+        else {
+            this->first = current->next;
+            delete current;
+        }
+    }
+    else
+        throw std::invalid_argument("Element does not in set");
 }
 
 template <class T>
 void set<T>::clear()
 {
-
+    if (this->first != nullptr) {
+        node<T>* current = this->first;
+        while (this->first != nullptr) {
+            this->first = current->next;
+            delete current;
+            current = this->first;
+        }
+        this->last = nullptr;
+    }
 }
 
 /* Операции множества */
@@ -118,7 +178,13 @@ set<T>& set<T>::set_substract(const set<T>& s) const
 template <class T>
 set<T>& set<T>::operator= (const set<T>& s)
 {
+    if (this != &s) {
+        this->clear();
+        for (Iterator<T> i = s.iterator_begin(); !i.is_end(); ++i)
+            this->add(i.value());
+    }
 
+    return *this;
 }
 
 template <class T>
@@ -142,7 +208,16 @@ set<T>& set<T>::operator/= (const set<T>& s)
 template <class _T>
 std::ostream& operator<< (std::ostream& os, const set<_T>& lst)
 {
+    if (lst.get_length() == 0) {
+        os << "Set is empty";
+        return os;
+    }
+    os << "[ ";
+    for (Iterator<_T> iterator = lst.iterator_begin(); !iterator.is_end(); ++iterator)
+            os << iterator.value() << " ";
+    os << "]";
 
+    return os;
 }
 
 template <class _T>
@@ -167,5 +242,5 @@ set<_T> operator/ (const set<_T>& s1, const set<_T>& s2)
 template <class T>
 set<T>::~set<T>()
 {
-
+    this->clear();
 }
